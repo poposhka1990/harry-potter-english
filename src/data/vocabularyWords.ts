80aed019a454005d3cd925db98fe10518c1fe863
Ilya Popov <popovemail1990@gmail.com>
2025-01-17 11:07:44 +0300

    add new words

--- src/data/vocabularyWords.ts
+++ src/data/vocabularyWords.ts
@@ -12,15 +12,57 @@ export const vocabularyWords = [
     partOfSpeech: "словосочетание"
   },
   {
+    word: "last",
+    translation: "последний",
+    example: "She was the last to finish.",
+    partOfSpeech: "прилагательное"
+  },
+  {
+    word: "expect",
+    translation: "ожидать",
+    example: "I expected you yesterday.",
+    partOfSpeech: "глагол"
+  },
+  {
+    word: "be involved in",
+    translation: "быть вовлеченным",
+    example: "The group is involved in Antarctic exploration.",
+    partOfSpeech: "прилагательное"
+  },
+  {
     word: "mysterious",
     translation: "загадочный",
     example: "They weren't involved in anything strange or mysterious.",
     partOfSpeech: "прилагательное"
   },
   {
+    word: "to hold with",
+    translation: "соглашаться, одобрять",
+    example: "He does not hold with theatres.",
+    partOfSpeech: "глагол"
+  },
+  {
     word: "nonsense",
     translation: "глупости",
     example: "They didn't hold with such nonsense.",
+    partOfSpeech: "существительное"
+  },
+  {
+    word: "nonsense",
+    translation: "глупости",
+    example: "They didn't hold with such nonsense.",
+    partOfSpeech: "существительное"
+  },
+  {
+    word: "call",
+    translation: "называть, звонить",
+    example: "He is called Tom.",
+    partOfSpeech: "глагол"
+  },
+  {
+    word: "drill",
+    translation: "дрель",
+    example: "Can you work an electric drill?",
     partOfSpeech: "существительное"
   },
   {
@@ -30,9 +72,105 @@ export const vocabularyWords = [
     partOfSpeech: "прилагательное"
   },
   {
+    word: "hardly",
+    translation: "едва",
+    example: "She is hardly twenty.",
+    partOfSpeech: "наречие"
+  },
+  {
+    word: "although",
+    translation: "хотя",
+    example: "I'll be there, although I may be late.",
+    partOfSpeech: "союз"
+  },
+  {
+    word: "mustache",
+    translation: "дрель",
+    example: "He decided to grow a mustache.",
+    partOfSpeech: "существительное"
+  },
+  {
+    word: "thin",
+    translation: "худая",
+    example: "He has long thin hands.",
+    partOfSpeech: "прилагательное"
+  },
+  {
+    word: "nearly twice the usual amount of neck",
+    translation: "шея почти вдвое длиннее, чем обычно",
+    example: "Mrs. Dursley has nearly twice the usual amount of neck.",
+    partOfSpeech: "словосочетание"
+  },
+  {
+    word: "nearly twice the usual amount of neck",
+    translation: "шея почти вдвое длиннее, чем обычно",
+    example: "Mrs. Dursley has nearly twice the usual amount of neck.",
+    partOfSpeech: "словосочетание"
+  },
+  {
+    word: "to come in useful",
+    translation: "пригодиться",
+    example: "My guitar skills came in very useful.",
+    partOfSpeech: "словосочетание"
+  },
+  {
+    word: "to spend time doing something",
+    translation: "проводить время делая что-то",
+    example: "He spends a lot of his time learning English.",
+    partOfSpeech: "словосочетание"
+  },
+  {
+    word: "fence",
+    translation: "забор, изгородь",
+    example: "They climbed through a hole in the fence.",
+    partOfSpeech: "существительное"
+  },
+  {
     word: "to spy on",
     translation: "шпионить",
     example: "She spent her time spying on the neighbors.",
     partOfSpeech: "глагол"
-  }
+  },
+  {
+    word: "neighbor",
+    translation: "сосед",
+    example: "We invited our friends and neighbors.",
+    partOfSpeech: "существительное"
+  },
+  {
+    word: "in my opinion",
+    translation: "по моему мнению",
+    example: "In my opinion, it's the greatest book.",
+    partOfSpeech: "словосочетание"
+  },
+  {
+    word: "in my opinion",
+    translation: "по моему мнению",
+    example: "In my opinion, it's the greatest book.",
+    partOfSpeech: "словосочетание"
+  },
+  {
+    word: "everything",
+    translation: "всё",
+    example: "Money isn't everything.",
+    partOfSpeech: "существительное"
+  },
+  {
+    word: "fear",
+    translation: "страх",
+    example: "Fear is written on his face.",
+    partOfSpeech: "существительное"
+  },
+  {
+    word: "would",
+    translation: "бы",
+    example: "It would seem like it was endless",
+    partOfSpeech: "вспомогательное слово"
+  },
+  {
+    word: "to discover",
+    translation: "открывать, раскрывать",
+    example: "Who discovered the North Pole?",
+    partOfSpeech: "глагол"
+  },
 ];
